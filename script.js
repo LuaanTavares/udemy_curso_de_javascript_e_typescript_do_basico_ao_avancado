538967cdcfb2319c8905ef7419f4d6c763d4a429
Luan Tavares <luankriok18@gmail.com>
2023-03-28 10:46:47 -0300

    Objeto Date

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//para saber a data atual
+// const data = new Date();
+// console.log(data.toString())
 
+// Para adicionar o zero na frente dos numeros menores que 10
+// function zeroAEsquerda (num){
+//   return num >= 10 ? num : `0${num}`
+// }
+
+//Como pegar os valores do Date
+// const data = new Date('2023-03-7 10:20:59');
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth() + 1); // Mês começa do zero
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('ms', data.getMilliseconds());
+// console.log('Dia semana', data.getDay()); // 0 = Domingo, 6 = Sábado
+// console.log(data.toString());
+
+//Convertendo para data do Brasil
+// const data = new Date();
+// data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short'});
