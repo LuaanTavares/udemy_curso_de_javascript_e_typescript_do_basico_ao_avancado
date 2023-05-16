886fc45934faedcabf5a6b69fc47704f280ee638
Luan Tavares <luankriok18@gmail.com>
2023-05-16 10:11:27 -0300

    fix

--- 4 _Math/script.js
+++ 4 _Math/script.js
@@ -3,7 +3,7 @@ let num1 = 9.54578
 let num2 = 5
 console.log(Math.floor(num1)) //Arredondar um número pra cima;
 console.log(Math.ceil(num1)) // Arredondar um número pra baixo;
-console.log(Math.round(num1)) // Arredondar para o numero inteiro mias próximo;
+console.log(Math.round(num1)) // Arredondar para o numero inteiro mais próximo;
 console.log(Math.max(num1, num2)) // Achar o maior valor;
 console.log(Math.min(num1, num2)) // Achar o menor valor;
 console.log(Math.random()) // Gerar número aleatório entre 0 e 1 (o 1 não é incluído);
